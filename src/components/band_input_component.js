f6dc9a14c4e47e72f906f5c13d46b41dcee2b49f
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-11-09 09:22:44 +0000

    automatically backed up by learn

--- src/components/band_input_component.js
+++ src/components/band_input_component.js
@@ -1,16 +1,16 @@
 import React from 'react';
- 
+
 export default class BandInput extends React.Component {
   constructor(props) {
     super(props)
     this.onSubmit = this.onSubmit.bind(this)
   }
- 
+
   onSubmit(e) {
     e.preventDefault()
     this.props.store.dispatch({type: 'ADD_BAND', payload: {title: 'New Band'}})
   }
- 
+
   render(){
     return(
       <div>
