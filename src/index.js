bd1e9328938f02c6dbfcc4a0d0edb2f6bd67290a
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-11-09 09:18:06 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from './store';
-import bandReducer from './reducers/band_reducer'
-const store = createStore(bandReducer)
  
-ReactDOM.render(<InsertTopLevelComponent />, document.getElementById('container'))
+import { createStore } from './store';
+ 
+import BandInput from './components/band_input_component';
+import bandReducer from './reducers/band_reducer.js';
+ 
+const store = createStore(bandReducer);
+ 
+ReactDOM.render(<BandInput />, document.getElementById('container'))
