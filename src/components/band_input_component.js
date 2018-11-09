14e6d1722704f6068426e0882eaf0e8bb6d6fd5d
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-11-09 09:19:40 +0000

    automatically backed up by learn

--- src/components/band_input_component.js
+++ src/components/band_input_component.js
@@ -1,3 +1,12 @@
+import React, { Component } from 'react'
+
 const store = createStore(bandReducer);
  
 ReactDOM.render(<BandInput store={store}/>, document.getElementById('container'))
+
+
+export default class BandInput extends React.Component {
+    render(){
+        return <div>I LIEK TEH BANDZ</div>
+    }
+}
