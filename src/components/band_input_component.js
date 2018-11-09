e0089bbde32035c857d03eecfbee04c05e885503
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-11-09 09:21:14 +0000

    automatically backed up by learn

--- src/components/band_input_component.js
+++ src/components/band_input_component.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react'
-
-const store = createStore(bandReducer);
-
-ReactDOM.render(<BandInput store={store}/>, document.getElementById('container'))
-
-
+import React from 'react';
+ 
 export default class BandInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+ 
+  onSubmit(e) {
+    e.preventDefault()
+    this.props.store.dispatch({type: 'ADD_BAND', payload: {title: 'New Band'}})
+  }
+ 
   render(){
-      return(
-          <div>
-              <form>
-                  <label>Enter Band: </label>
-                  <input />
-                  <button type="submit">Submit</button>
-              </form>
-          </div>
-      )
+    return(
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <label>Enter Band: </label>
+          <input />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    )
   }
 }
